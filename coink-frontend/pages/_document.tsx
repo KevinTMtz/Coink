becc61484fb8afc0833f89d1c64fa811d42619ef
MarioJim <mario.emilio.j@gmail.com>
2021-05-18 12:34:06 -0500

    Add font to global styles

---
+++ coink-frontend/pages/_document.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta name="description" content="Personal financial manager" />
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@200;300;400&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
