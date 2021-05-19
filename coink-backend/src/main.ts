bcfb38bdd061d42c269fd81857adf53e09d963ca
MarioJim <mario.emilio.j@gmail.com>
2021-05-19 08:37:03 -0500

    Create auth module and login/signup functions

--- coink-backend/src/main.ts
+++ coink-backend/src/main.ts
@@ -1,16 +1,51 @@
 import dotenv from 'dotenv';
+import MongoStore from 'connect-mongo';
 import express from 'express';
+import session from 'express-session';
+import mongoose from 'mongoose';
+import { exit } from 'process';
 
 dotenv.config({ path: '.env.development' });
+if (!process.env.MONGODB_URI) {
+  console.error('No MongoDB connection specified');
+  exit(1);
+}
+const mongoUrl = process.env.MONGODB_URI;
+
+import authRouter from './routers/auth';
+
+declare module 'express-session' {
+  interface SessionData {
+    user: string;
+  }
+}
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+// Bodyparser middleware
+app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`coink server listening at http://localhost:${port}`);
-  console.log(`connecting to mongodb on ${process.env.MONGODB_URI}`);
-});
+// Session middleware
+app.use(
+  session({
+    secret: process.env.SESSIONS_SECRET || '',
+    store: MongoStore.create({ mongoUrl }),
+    resave: false,
+    saveUninitialized: true,
+  }),
+);
+
+app.use('/api/auth', authRouter);
+
+(async () => {
+  await mongoose.connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  app.listen(port, () => {
+    console.log(`coink server listening at http://localhost:${port}`);
+    console.log(`connecting to database on ${mongoUrl}`);
+  });
+})();
