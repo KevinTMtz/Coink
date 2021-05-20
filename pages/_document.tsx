a372eda9b420df9141573127dbb618bf47b27887
MarioJim <mario.emilio.j@gmail.com>
2021-05-20 13:25:00 -0500

    Move global styles to MyDocument

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { Global } from '@emotion/react';
 
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html lang="en" style={{ padding: 0, margin: 0 }}>
         <Head>
           <meta name="description" content="Personal financial manager" />
           <link rel="icon" href="/favicon.ico" />
@@ -15,7 +16,18 @@ class MyDocument extends Document {
           />
         </Head>
 
-        <body>
+        <Global
+          styles={{
+            '*': {
+              boxSizing: 'border-box',
+              fontFamily:
+                'Work Sans, -apple-system, BlinkMacSystemFont, Segoe UI, Ubuntu, Fira Sans, Helvetica Neue, sans-serif !important',
+              fontWeight: 300,
+            },
+          }}
+        />
+
+        <body style={{ padding: 0, margin: 0 }}>
           <Main />
           <NextScript />
         </body>
