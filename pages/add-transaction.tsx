f2bdc65b11e7c3ec7685dc44f7cdc5da31c35e3b
MarioJim <mario.emilio.j@gmail.com>
2021-05-27 17:51:24 -0500

    Fetch initial fields from EditTransactionPage

--- pages/add-transaction.tsx
+++ pages/add-transaction.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
 
-import Form from '../components/TransactionForm';
+import Form, { TransactionFields } from '../components/TransactionForm';
 
 const AddTransactionPage: React.FC = () => {
-  return <Form action='add' />;
+  const [transaction, setTransaction] = useState<TransactionFields>({
+    type: 'income',
+    category: '',
+    name: '',
+    comments: '',
+    amount: 0.0,
+    date: new Date(),
+  });
+
+  const router = useRouter();
+
+  return (
+    <Form
+      action='add'
+      transaction={transaction}
+      setTransaction={setTransaction}
+      router={router}
+    />
+  );
 };
 
 export default AddTransactionPage;
