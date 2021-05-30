3b843592a1e8ca8bbbadc1e703687b10fa45d403
MarioJim <mario.emilio.j@gmail.com>
2021-05-29 23:02:47 -0500

    Create landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,192 +1,135 @@
 import React from 'react';
-import { css } from '@emotion/react';
+/** @jsxImportSource @emotion/react */ import { css } from '@emotion/react';
+import { Button } from '@geist-ui/react';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 
-const sth = css({});
+import Title from '../components/Title';
 
-const HomePage: React.FC = () => (
-  <div className='container' css={sth}>
-    <main>
-      <h1 className='title'>
-        Welcome to <b>coink</b>
-      </h1>
+const ContainerStyle = css({
+  minHeight: '100vh',
+  maxWidth: '900px',
+  margin: '0 auto',
+  padding: '20px',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+});
 
-      <p className='description'>
-        Get started by editing <code>pages/index.js</code>
-      </p>
+const HeaderStyle = css({
+  display: 'flex',
+  alignItems: 'center',
+  img: {
+    height: '100px',
+  },
+  '@media (max-width: 720px)': {
+    img: {
+      height: '80px',
+    },
+  },
+});
 
-      <div className='grid'>
-        <a href='https://nextjs.org/docs' className='card'>
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+const ButtonStyle = css({
+  margin: '12px',
+  'div.text': {
+    fontSize: '1.2rem',
+    fontWeight: 500,
+  },
+});
 
-        <a href='https://nextjs.org/learn' className='card'>
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
+const FeatureContainerStyle = css({
+  display: 'flex',
+  flexWrap: 'wrap',
+  justifyContent: 'space-evenly',
+});
 
-        <a
-          href='https://github.com/vercel/next.js/tree/master/examples'
-          className='card'
+const FeatureDivStyle = css({
+  maxWidth: '300px',
+  margin: '0 20px 12px',
+  img: {
+    display: 'block',
+    height: '120px',
+    margin: '10px auto',
+  },
+  h3: {
+    margin: 0,
+    fontWeight: 500,
+    textAlign: 'center',
+  },
+  p: {
+    margin: 0,
+    textAlign: 'center',
+  },
+  '@media (max-width: 480px)': {
+    img: {
+      height: '25vw',
+    },
+  },
+});
+
+const HomePage: React.FC = () => {
+  const router = useRouter();
+  return (
+    <>
+      <Head>
+        <title>coink</title>
+      </Head>
+      <div css={ContainerStyle}>
+        <header css={HeaderStyle}>
+          <img src='/coinkLogo.svg' alt='coink logo' />
+          <Title>coink</Title>
+        </header>
+
+        <Button
+          onClick={() => router.push('/login')}
+          type='success-light'
+          size='large'
+          css={ButtonStyle}
         >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
+          Regístrate
+        </Button>
 
-        <a
-          href='https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          className='card'
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-        </a>
+        <div css={FeatureContainerStyle}>
+          <div css={FeatureDivStyle}>
+            <img src='/images/bank.svg' />
+            <h3>Organiza tus finanzas</h3>
+            <p>
+              Administrar tu dinero puede ser tedioso, coink te ayuda a juntar
+              todos tus gastos e ingresos en un solo lugar
+            </p>
+          </div>
+
+          <div css={FeatureDivStyle}>
+            <img src='/images/education.svg' />
+            <h3>Aprende algo nuevo</h3>
+            <p>
+              Visualiza tus hábitos de gasto, tus hábitos de ahorro y tus
+              ingresos fácilmente - sin complicadas hojas de cálculo
+            </p>
+          </div>
+
+          <div css={FeatureDivStyle}>
+            <img src='/images/analysis.svg' />
+            <h3>Optimiza tu dinero</h3>
+            <p>
+              Nuestras herramientas están diseñadas para ayudarle a ver su
+              dinero con claridad y tomar decisiones financieras
+            </p>
+          </div>
+
+          <div css={FeatureDivStyle}>
+            <img src='/images/smartphone.svg' />
+            <h3>Crea una cuenta gratis</h3>
+            <p>
+              Regístrate en coink con sólo tu correo y disfruta de nuestro sitio
+              web desde tu navegador o tu teléfono
+            </p>
+          </div>
+        </div>
       </div>
-    </main>
+    </>
+  );
+};
 
-    <footer>
-      <a
-        href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        Powered by <img src='/vercel.svg' alt='Vercel Logo' className='logo' />
-      </a>
-    </footer>
-
-    <style jsx>{`
-      .container {
-        min-height: 100vh;
-        padding: 0 0.5rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      main {
-        padding: 5rem 0;
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer {
-        width: 100%;
-        height: 100px;
-        border-top: 1px solid #eaeaea;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer img {
-        margin-left: 0.5rem;
-      }
-
-      footer a {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
-
-      .title a {
-        color: #0070f3;
-        text-decoration: none;
-      }
-
-      .title a:hover,
-      .title a:focus,
-      .title a:active {
-        text-decoration: underline;
-      }
-
-      .title {
-        margin: 0;
-        line-height: 1.15;
-        font-size: 4rem;
-      }
-
-      .title,
-      .description {
-        text-align: center;
-      }
-
-      .subtitle {
-        font-size: 2rem;
-      }
-
-      .description {
-        line-height: 1.5;
-        font-size: 1.5rem;
-      }
-
-      code {
-        background: #fafafa;
-        border-radius: 5px;
-        padding: 0.75rem;
-        font-size: 1.1rem;
-        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-      }
-
-      .grid {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-wrap: wrap;
-
-        max-width: 800px;
-        margin-top: 3rem;
-      }
-
-      .card {
-        margin: 1rem;
-        flex-basis: 45%;
-        padding: 1.5rem;
-        text-align: left;
-        color: inherit;
-        text-decoration: none;
-        border: 1px solid #eaeaea;
-        border-radius: 10px;
-        transition: color 0.15s ease, border-color 0.15s ease;
-      }
-
-      .card:hover,
-      .card:focus,
-      .card:active {
-        color: #0070f3;
-        border-color: #0070f3;
-      }
-
-      .card h3 {
-        margin: 0 0 1rem 0;
-        font-size: 1.5rem;
-      }
-
-      .card p {
-        margin: 0;
-        font-size: 1.25rem;
-        line-height: 1.5;
-      }
-
-      .logo {
-        height: 1em;
-      }
-
-      @media (max-width: 600px) {
-        .grid {
-          width: 100%;
-          flex-direction: column;
-        }
-      }
-    `}</style>
-  </div>
-);
 export default HomePage;
