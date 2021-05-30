a03068c77538289c49deac13c9496fd759918530
MarioJim <mario.emilio.j@gmail.com>
2021-05-29 23:02:21 -0500

    CSS fixes to Title, TransactionCell and LoginPage

--- components/Title.tsx
+++ components/Title.tsx
@@ -2,13 +2,13 @@ import styled from '@emotion/styled';
 
 const Title = styled.h1({
   lineHeight: 1.15,
-  fontSize: '5vw',
+  fontSize: '4rem',
   textAlign: 'center',
   margin: 0,
   padding: '0.5rem',
   width: '100%',
-  '@media (max-width: 600px)': {
-    fontSize: '12vw',
+  '@media (max-width: 720px)': {
+    fontSize: '3rem',
   },
 });
 
--- components/TransactionCell.tsx
+++ components/TransactionCell.tsx
@@ -11,13 +11,19 @@ const CardStyle = css({
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'space-between',
+  flex: '0 0',
   h1: { fontSize: '1.5rem' },
   h2: { fontSize: '1.25rem', fontWeight: 500 },
   h3: { fontSize: '1rem', fontWeight: 300 },
   width: '100%',
   cursor: 'pointer',
-  '@media (max-width: 600px)': {
-    h2: { width: 'auto' },
+  '@media (max-width: 720px)': {
+    h2: { fontSize: '18px', width: 'auto' },
+    h3: { fontSize: '15px' },
+  },
+  '@media (max-width: 480px)': {
+    h2: { fontSize: '16px', width: 'auto' },
+    h3: { fontSize: '13px' },
   },
 });
 
@@ -56,7 +62,9 @@ const TransactionCell: React.FC<TransactionProps> = ({
         <div css={InfoStyle}>
           <TransactionIcon type={data.type} category={data.category} />
           <div>
-            <h2 style={{ margin: '0 0 6px 0' }}>{data.name}</h2>
+            <h2 style={{ margin: '0 0 6px 0', wordBreak: 'break-word' }}>
+              {data.name}
+            </h2>
             <h3 css={NoMargin}>{dateFormatter.format(new Date(data.date))}</h3>
           </div>
         </div>
